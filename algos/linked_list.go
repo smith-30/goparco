fc0493df2ae133bfe27298255fa1aa0d32e7a6f8
smith-30 <parkour12019@gmail.com>
2020-03-29 20:16:39 +0900

    mod:

--- algos/linked_list.go
+++ algos/linked_list.go
@@ -1,14 +1,35 @@
 package main
 
+import (
+	"fmt"
+)
+
 type Node struct {
 	property int
 	nextNode *Node
 }
 
 type LinkedList struct {
-	headNode *Node	
+	headNode *Node
+}
+
+func (a *LinkedList) AddToHead(p int) {
+	n := Node{
+		property: p,
+	}
+
+	if a.headNode != nil {
+		n.nextNode = a.headNode
+	}
+	a.headNode = &n
 }
 
 func main() {
+	l := LinkedList{}
+	l.AddToHead(1)
+	l.AddToHead(3)
+	fmt.Printf("%#v\n", l.headNode.property)
 
+	fmt.Printf("%#v\n", l.headNode)
+	fmt.Printf("%#v\n", l.headNode.nextNode)
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/smith-30/goparco
 go 1.13
 
 require (
-	github.com/aws/aws-sdk-go v1.27.0
 	github.com/DATA-DOG/go-sqlmock v1.4.1
+	github.com/aws/aws-sdk-go v1.27.0
 	github.com/jinzhu/gorm v1.9.11
 )
